e96ef2819d3e226b5d0c34605228e752036172d4
miyoooon <133766145+miyoooon@users.noreply.github.com>
2023-06-04 21:57:19 +0000

    [Finish] 3-4

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
+  get 'lists/:id' => 'lists#show', as: 'list'
   get '/top' => 'homes#top'
 end
