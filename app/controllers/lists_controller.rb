eea7b4d926f9a2252da1caf2e2823be2bd1cdecb
miyoooon <133766145+miyoooon@users.noreply.github.com>
2023-06-07 23:45:48 +0000

    [Finish] 3-12

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,9 +6,11 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
+      flash[:notice] = "投稿が成功しました"
       redirect_to list_path(@list.id)
     else
       render :new
+    end
   end
 
   def index
